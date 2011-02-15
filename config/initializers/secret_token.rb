c9e6af901d16468943b284c4b48177eb85257d63
unknown <Vince@.(none)>
2011-02-14 21:53:31 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Meeve::Application.config.secret_token = '9f7d304a70aa16ac36d4f47c16719a8b1bc816a2ae85ba2e0821fda8d05cc2e10094858befa92670e690ce63fcb5da5256618484e121558a586ed234249173c3'
